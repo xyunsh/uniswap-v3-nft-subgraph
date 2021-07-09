46bceeaf6a6a1d57af9a7e5ac59a2c4ab11ecc39
shenyj <xyunsh@gmail.com>
2021-07-09 16:23:24 +0800

    builded

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,79 +1,67 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts";
 import {
-  NonfungiblePositionManager,
-  Approval,
-  ApprovalForAll,
-  Collect,
-  DecreaseLiquidity,
-  IncreaseLiquidity,
-  Transfer
-} from "../generated/NonfungiblePositionManager/NonfungiblePositionManager"
-import { ExampleEntity } from "../generated/schema"
+    NonfungiblePositionManager,
+    Approval,
+    ApprovalForAll,
+    Collect,
+    DecreaseLiquidity,
+    IncreaseLiquidity,
+    Transfer,
+} from "./types/NonfungiblePositionManager/NonfungiblePositionManager";
+import { Transaction, TxEvent } from "./types/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.PERMIT_TYPEHASH(...)
-  // - contract.WETH9(...)
-  // - contract.balanceOf(...)
-  // - contract.baseURI(...)
-  // - contract.factory(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.positions(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
+export function loadTransaction(event: ethereum.Event): Transaction {
+    let transaction = Transaction.load(event.transaction.hash.toHexString());
+    if (transaction === null) {
+        transaction = new Transaction(event.transaction.hash.toHexString());
+    }
+    transaction.blockNumber = event.block.number;
+    transaction.timestamp = event.block.timestamp;
+    transaction.gasUsed = event.transaction.gasUsed;
+    transaction.gasPrice = event.transaction.gasPrice;
+    transaction.save();
+    return transaction as Transaction;
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+function saveTxEvent(
+    name: string,
+    event: ethereum.Event,
+    params: string
+): void {
+    let tx = loadTransaction(event);
+    let e = new TxEvent(`${tx.id}#${event.logIndex}`);
+    e.name = name;
+    e.params = params;
+    e.transaction = tx.id;
+    e.save();
+}
 
-export function handleCollect(event: Collect): void {}
+export function handleApproval(event: Approval): void {
+    const json = `{"owner":"${event.params.owner}", "approved":"${event.params.approved}", "tokenId":"${event.params.tokenId}"}`;
+    saveTxEvent("approval", event, json);
+}
 
-export function handleDecreaseLiquidity(event: DecreaseLiquidity): void {}
+export function handleApprovalForAll(event: ApprovalForAll): void {
+    const json = `{"owner":"${event.params.owner}","operator":"${event.params.operator}","approved":"${event.params.approved}"}`;
+    saveTxEvent("approvalForAll", event, json);
+}
 
-export function handleIncreaseLiquidity(event: IncreaseLiquidity): void {}
+export function handleCollect(event: Collect): void {
+    const json = `{"tokenId":"${event.params.tokenId}","recipient":"${event.params.recipient}","amount0":"${event.params.amount0}","amount1":"${event.params.amount1}"}`;
+    saveTxEvent("collect", event, json);
+}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleDecreaseLiquidity(event: DecreaseLiquidity): void {
+    const json = `{"tokenId":"${event.params.tokenId}","liquidity":"${event.params.liquidity}","amount0":"${event.params.amount0}","amount1":"${event.params.amount1}"}`;
+    saveTxEvent("decreaseLiquidity", event, json);
+}
+
+export function handleIncreaseLiquidity(event: IncreaseLiquidity): void {
+    const json = `{"tokenId":"${event.params.tokenId}","liquidity":"${event.params.liquidity}","amount0":"${event.params.amount0}","amount1":"${event.params.amount1}"}`;
+    saveTxEvent("increaseLiquidity", event, json);
+}
+
+export function handleTransfer(event: Transfer): void {
+    const json = `{"from":"${event.params.from}","to":"${event.params.to}","tokenId":"${event.params.tokenId}"}`;
+    saveTxEvent("transfer", event, json);
+}
